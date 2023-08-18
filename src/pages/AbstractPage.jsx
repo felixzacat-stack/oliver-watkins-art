fa6e7107e181cbd2c035f7d981b74f4322a19f39
oliverwatkins <oliver.f.watkins@gmail.com>
2023-08-18 18:07:19 +0200

    asdfasdf35fasdasdfasdf

--- src/pages/AbstractPage.jsx
+++ src/pages/AbstractPage.jsx
@@ -3,16 +3,7 @@ import React from 'react';
 import "./AbstractPage.css"
 import img_rebirth from "../images/ac61.jpg";
 import img_summer_party from "../images/summerparty.jpg";
-import img_dogs from "../images/dogs.jpg";
-import str_parad_2 from "../images/ac29.jpg";
 import windmill from "../images/ac53.jpg";
-import img_cat_dinner from "../images/catdinner.jpg";
-import i7 from "../images/ac56.jpg";
-import i8 from "../images/ac57.jpg";
-import i9 from "../images/ac58.jpg";
-import i10 from "../images/ac59.jpg";
-import str_para_1 from "../images/ac33.jpg";
-import bar from "../images/bar.jpg";
 
 
 function AbstractPage(props) {
