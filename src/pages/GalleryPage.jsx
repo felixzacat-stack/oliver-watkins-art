72a2bc4bff135b640e22a9d80bef30016d78bcce
oliverwatkins <oliver.f.watkins@gmail.com>
2024-02-11 16:08:30 +0100

    images

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -14,6 +14,8 @@ import windmill_mu1 from "../images/windmill_mu1.png";
 import abstr_greece from "../images/abstract_greece_COMP.jpg";
 import bar from "../images/bar_80_by_60_COMP.jpg";
 import agnes from "../images/agnes_COMP.jpg";
+import agnes_mu1 from "../images/agnes_mu1.jpg";
+
 import broken_car from "../images/broken_car_70_by_50_COMP.jpg";
 import cat_dinner from "../images/cat_dinner_50_by_60_COMP.jpg";
 import dogs from "../images/dogs_COMP.jpg";
@@ -21,6 +23,7 @@ import dogs_mu1 from "../images/dogs_mu1.jpg";
 import figure from "../images/figure_COMP.jpg";
 import khole from "../images/khole_60_by_45_COMP.jpg";
 import lady1 from "../images/lady_1_COMP.jpg";
+import lady1_mu1 from "../images/lady_1_mu1.jpg";
 import lady2 from "../images/lady_2_COMP.jpg";
 import lady3 from "../images/lady_3_COMP.jpg";
 import lady3_mu1 from "../images/lady3_mu1.png";
@@ -34,6 +37,8 @@ import paradise1 from "../images/paradise_1_COMP.jpg";
 import paradise2 from "../images/paradise_2_COMP.jpg";
 import paradise3 from "../images/paradise_3_COMP.jpg";
 import perm_vacation from "../images/perm_vacation.jpg";
+import perm_vacation_mu1 from "../images/perm_vacation_mu1.jpg";
+
 import queens_gambit from "../images/queens_gambit_COMP.jpg";
 import shapes from "../images/shapes_COMP.jpg";
 import shapes_mu1 from "../images/shapes_mu1.jpg";
@@ -43,43 +48,43 @@ import orange from "../images/orange_COMP.jpg";
 import blue from "../images/blue_COMP.jpg";
 
 
-
-
 import Modal from 'react-modal';
 import GalleryModal from "../common/GalleryModal";
 import Masonry from "react-responsive-masonry";
 // import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 
 
-
 let pics = [{
+    title: "ABSTRACT UNTITLED -",
+    img: [img_rebirth, img_rebirth_mu1,
+        img_rebirth_mu2],
+    spec: "Acrylic on stretched canvas ",
+    dimensions: "(80cm by 60cm)",
+    price: "250€ + PPH"
 
-        title: "Rebirth -",
-        img: [img_rebirth,    img_rebirth_mu1,
-            img_rebirth_mu2],
-        spec: "Acrylic on stretched canvas ",
-        dimensions: "(80cm by 60cm)",
+}, {
 
-    }, {
+    title: "Agnes - ",
+    img: [agnes, agnes_mu1],
+    spec: "Acrylic on paper ",
+    dimensions: "(60cm by 45cm)",
+    price: "100€ + PPH (frame included)"
+},
+    {
 
-        title: "Agnes - ",
-        img: [agnes],
-        spec: "Acrylic on canvas board",
-        dimensions: "(60cm by 45cm)",
-
-    }, {
 
         title: "Summer Party -",
         img: [img_summer_party, img_summer_party_mu1],
         spec: "Acrylic on stretched canvas ",
         dimensions: "(80cm by 60cm)",
-
+        price: "250€ + PPH"
     }, {
 
         title: "Windmill -",
         img: [windmill, windmill_mu1],
         spec: "Acrylic on canvas board (60cm by 45cm) ",
         dimensions: "(60cm by 45cm)",
+        price: "80€ + PPH"
 
     }, {
 
@@ -87,25 +92,29 @@ let pics = [{
         img: [abstr_greece],
         spec: "Acrylic on paper ",
         dimensions: "(xxx)",
+        price: "50€ + PPH"
     },
+
     {
-        title: "Beasts at the Bar -",
-        img: [bar],
+        title: "Pink ",
+        img: [pink],
         spec: "Acrylic on stretched canvas ",
         dimensions: "(80cm by 60cm)",
-    },
-    {
+        price: "150€ + PH",
+    }, {
         title: "Paris Texas -",
         img: [broken_car],
         spec: "Acrylic on stretched canvas ",
         dimensions: "(80cm by 60cm)",
+        price: "150€ + PPH"
 
     }, {
 
-        title: "Dinner - ",
+        title: "Dinner with Cat - ",
         img: [cat_dinner],
         spec: "Acrylic on stretched canvas ",
         dimensions: "(80cm by 60cm)",
+        price: "100€ + PPH"
     },
     {
 
@@ -121,30 +130,40 @@ let pics = [{
         img: [figure],
         spec: "Acrylic on stretched canvas ",
         dimensions: "(80cm by 60cm)",
-
+        price: "50€ + PPH"
     },
-
     {
 
         title: "K-Hole of the spotless mind - ",
         img: [khole],
         spec: "Acrylic on stretched canvas ",
         dimensions: "(80cm by 60cm)",
+        price: "100€ + PH"
 
     }, {
 
         title: "Lady on Sofa -",
-        img: [lady1],
+        img: [lady1, lady1_mu1],
         spec: "Acrylic on canvas board ",
         dimensions: "(60cm by 45cm)",
+        price: "150€ + PH (frame included)"
 
+    },
+    {
+
+        title: "Blue ",
+        img: [blue],
+        spec: "Acrylic on stretched canvas ",
+        dimensions: "(80cm by 60cm)",
+        price: "150€ + PH",
     },
     {
 
         title: "Figure #2 - ",
         img: [lady2],
-        spec: "Acrylic on stretched canvas ",
+        spec: "Acrylic on paper ",
         dimensions: "(80cm by 60cm)",
+        price: "30€ + PH",
 
     },
     {
@@ -169,7 +188,7 @@ let pics = [{
         img: [monster],
         spec: "Acrylic on stretched canvas ",
         dimensions: "(80cm by 60cm)",
-
+        price: "40€ + PH",
     },
     {
 
@@ -185,6 +204,14 @@ let pics = [{
         spec: "Acrylic on paper ",
         dimensions: "(60cm by 45cm)",
 
+    },
+
+    {
+        title: "Beasts at the Bar -",
+        img: [bar],
+        spec: "Acrylic on stretched canvas ",
+        dimensions: "(80cm by 60cm)",
+        price: "250€ + PPH"
     },
     {
 
@@ -204,10 +231,10 @@ let pics = [{
     {
 
         title: "The literary boor - ",
-        img: [perm_vacation],
-        spec: "Acrylic on canvas board  ",
+        img: [perm_vacation, perm_vacation_mu1],
+        spec: "Acrylic on paper   ",
         dimensions: "(60cm by 45cm)",
-
+        price: "80€ + PH (frame included)",
     },
     {
 
@@ -215,7 +242,7 @@ let pics = [{
         img: [queens_gambit],
         spec: "Acrylic on canvas board  ",
         dimensions: "(60cm by 45cm)",
-
+        price: "50€ + PH",
     },
     {
 
@@ -224,26 +251,13 @@ let pics = [{
         dimensions: "(60cm by 45cm)",
     },
     {
-
-        title: "Pink ",
-        img: [pink],
-        spec: "Acrylic on stretched canvas ",
-        dimensions: "(80cm by 60cm)",
-    },
-    {
-
         title: "Orange ",
         img: [orange],
         spec: "Acrylic on stretched canvas ",
         dimensions: "(80cm by 60cm)",
+        price: "150€ + PH",
     },
-    {
 
-        title: "Blue ",
-        img: [blue],
-        spec: "Acrylic on stretched canvas ",
-        dimensions: "(80cm by 60cm)",
-    },
 ]
 
 
@@ -267,18 +281,17 @@ export default function GalleryPage() {
 
     return (
 
-        <div className={"gallery-container container"} >
+        <div className={"gallery-container container"}>
             {/*<ResponsiveMasonry*/}
             {/*    columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}*/}
             {/*>*/}
 
 
+            <Masonry
+                // columnsCount={3} gutter="10px"
+            >
 
-        <Masonry
-            // columnsCount={3} gutter="10px"
-        >
-
-                 {isOpen && (
+                {isOpen && (
                     <GalleryModal
                         img={imgModal}
                         onClick={() => closeModal()}
@@ -286,7 +299,7 @@ export default function GalleryPage() {
                     >
                     </GalleryModal>
                 )}
-            {
+                {
                     pics && pics.map(el =>
                         <div className="gallery-grid-item">
                             <img onClick={() => openModal(el.img)} src={el.img[0]} alt="logo"/>
@@ -294,13 +307,14 @@ export default function GalleryPage() {
                                 <div className={"gallery-text1"}>{el.title}</div>
                                 <div className={"gallery-text2"}>{el.spec}</div>
                                 <div className={"gallery-text3"}>{el.dimensions}</div>
+                                {el.price && <div className={"gallery-text4"}>{el.price}</div>}
                             </div>
                             {el.sold &&
-                            <h4 className="shape-shop-image-sash"> &nbsp;  &nbsp;  &nbsp;     SOLD</h4>}
+                                <h4 className="shape-shop-image-sash"> &nbsp;  &nbsp;  &nbsp;     SOLD</h4>}
                         </div>
                     )
                 }
-        </Masonry>
+            </Masonry>
             {/*</ResponsiveMasonry>*/}
 
         </div>
