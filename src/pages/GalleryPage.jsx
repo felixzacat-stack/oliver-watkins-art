a33933db22ff266ade223dd0220f271dd12ed367
oliverwatkins <oliver.f.watkins@gmail.com>
2026-05-10 15:21:33 +0200

    navigate on image

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,50 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { Link } from 'react-router';
-import Modal from 'react-modal';
 import Masonry from "react-responsive-masonry";
 
 import "./GalleryPage.scss"
 import pics from 'src/data/pics';
-import GalleryModal from "../common/GalleryModal";
 
 export default function GalleryPage({ category = "all" }) {
 
     const label = category === "all" ? "Gallery" : category.charAt(0).toUpperCase() + category.slice(1);
     document.title = `${label} | Oliver Watkins Art`;
 
-    const [isOpen, setIsOpen] = useState(false);
-    const [imgModal, setImgModal] = useState();
-
     const filteredPics = category === "all"
         ? pics
         : pics.filter(p => p.category.includes(category));
 
-    function openModal(img) {
-        setImgModal(img)
-        setIsOpen(true);
-    }
-
-    function closeModal() {
-        setIsOpen(false);
-    }
-
-    useEffect(() => {
-        Modal.setAppElement('.gallery-container');
-    });
-
     return (
         <div className={"gallery-container container"}>
             <Masonry>
-                {isOpen && (
-                    <GalleryModal
-                        img={imgModal}
-                        onClick={() => closeModal()}
-                        closeModal={closeModal}
-                    />
-                )}
                 {filteredPics && filteredPics.map(el =>
                     <div className="gallery-grid-item" key={el.slug}>
-                        <img onClick={() => openModal(el.img)} src={el.img[0]} alt={el.title || "Artwork"} loading="lazy"/>
+                        <Link to={`/gallery/detail/${el.slug}`}>
+                            <img src={el.img[0]} alt={el.title || "Artwork"} loading="lazy"/>
+                        </Link>
                         <Link to={`/gallery/detail/${el.slug}`} className="gallery-picture-description">
                             <div className={"gallery-text1"}>{el.title}</div>
                             <div className={"gallery-text2"}>{el.spec}</div>
